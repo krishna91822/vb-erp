22f744d31598e3e668da9c872a534e73e93a9c4a
Aquib Nazmi <aquib.n@valuebound.com>
2021-11-24 16:32:19 +0530

    restructured redux store for cms

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import contentsSlice from "./contents-slice";
 import uiSlice from "./ui-slice";
 import POSOW_Slice from "./CMS/POSOW-slice";
-// import mainSlice from "./CMS/CMSpoSow-main-slices";
 
 export default configureStore({
   reducer: {
     contents: contentsSlice.reducer,
     ui: uiSlice.reducer,
     CMS_state: POSOW_Slice.reducer,
-    // poSOWTabViewState: mainSlice.reducer,
   },
 });
