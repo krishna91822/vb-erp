2c71becea133a4bba8f95db74e05dab48e6bbce8
Sachin Nishad <nishadsachin8898@gmail.com>
2021-12-13 12:30:25 +0530

    VET-64|sachin|refactered the code

--- src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import employeeReducer from './employee/employee.reducer';
-import uiReducer from './ui/ui.reducer';
-
-export default combineReducers({
-  employee: employeeReducer,
-  ui: uiReducer,
-});
