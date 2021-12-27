05ed10e53efcdb00a9319d49799d2d6b763d5ac1
Nikhil Jindal <nikhiljindal@Nikhils-MacBook-Air.local>
2021-12-27 10:48:37 +0530

    Integrated Launch API

--- src/components/employees/EmployeesList.js
+++ src/components/employees/EmployeesList.js
@@ -7,11 +7,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { searchEmployeeData } from "../../store/employees-actions";
 import { updateRewardEmployeeIdArray } from "../../store/rewards-actions";
 import { toast } from "react-toastify";
+import { color } from "@mui/system";
 
 const EmployeesList = (props) => {
   var collectedDataArray = [];
   const rows = [];
   var rowData;
+  var collectedDataSingleObject = {};
+  var tickedEmployeeId = "";
 
   const dispatch = useDispatch();
 
@@ -39,53 +42,92 @@ const EmployeesList = (props) => {
     const data = e.target.value;
     dispatch(searchEmployeeData(data));
   };
-
   const getSelectedRowsData = () => {
-    rowData = collectedDataArray;
-    collectedDataArray = [];
-    props.setOpenPopup(false);
     let employees_id = [];
-    if (props.receiver) {
-      props.updaterecipientsData(rowData);
-      toast.success("Reward Receivers Added");
-    }
-    if (props.sender) {
-      props.updateSenderData(rowData);
-      toast.success("Reward Sender Added");
-    }
-    rowData.forEach((data) => {
-      employees_id.push(data.employee_id);
-    });
-    const dataIds = {
-      recipients_ids: employees_id,
-    };
-    if (props.rewardList) {
-      dispatch(updateRewardEmployeeIdArray(dataIds, props.rewardId));
+    if (props.stateOfSelection === "multiple") {
+      rowData = collectedDataArray;
+      collectedDataArray = [];
+      props.setOpenPopup(false);
+
+      if (props.receiver) {
+        props.updaterecipientsData(rowData);
+        toast.success("Reward Receivers Added");
+      }
+
+      rowData.forEach((data) => {
+        employees_id.push(data.employee_id);
+      });
+      const dataIds = {
+        recipients_ids: employees_id,
+      };
+      if (props.rewardList) {
+        dispatch(updateRewardEmployeeIdArray(dataIds, props.rewardId));
+      }
+    } else {
+      var tempArray = [];
+      tempArray.push(collectedDataSingleObject);
+      collectedDataSingleObject = {};
+      props.setOpenPopup(false);
+      if (props.sender) {
+        props.updateSenderData(tempArray);
+        toast.success("Reward Sender Added");
+      }
+      tempArray.forEach((data) => {
+        employees_id.push(data.employee_id);
+      });
+      const dataIds = {
+        recipients_ids: employees_id,
+      };
+      if (props.rewardList) {
+        dispatch(updateRewardEmployeeIdArray(dataIds, props.rewardId));
+      }
     }
   };
 
-  const onRowClicked = (rowData, rowState) => {
-    let rowStatus = false;
-    let rowEmployeeId;
-    var collectedDataObject = {};
-    collectedDataObject.name = rowData.row.name;
-    collectedDataObject.email = rowData.row.email;
-    collectedDataObject.employee_id = rowData.row.employee_id;
-    for (var i = 0; i < collectedDataArray.length; i++) {
-      if (
-        collectedDataObject.employee_id === collectedDataArray[i].employee_id
-      ) {
-        rowStatus = true;
-        rowEmployeeId = collectedDataObject.employee_id;
+  const onRowClicked = (rowData, rowIndex) => {
+    if (props.stateOfSelection == "multiple") {
+      let rowStatus = false;
+      let rowEmployeeId;
+      var collectedDataObject = {};
+      collectedDataObject.name = rowData.row.name;
+      collectedDataObject.email = rowData.row.email;
+      collectedDataObject.employee_id = rowData.row.employee_id;
+      for (var i = 0; i < collectedDataArray.length; i++) {
+        if (
+          collectedDataObject.employee_id === collectedDataArray[i].employee_id
+        ) {
+          rowStatus = true;
+          rowEmployeeId = collectedDataObject.employee_id;
+        }
       }
-    }
-
-    if (rowStatus === true) {
-      collectedDataArray = collectedDataArray.filter(
-        (data) => data.employee_id !== rowEmployeeId
-      );
+      if (rowStatus === true) {
+        rowData.row.selected = "";
+        collectedDataArray = collectedDataArray.filter(
+          (data) => data.employee_id !== rowEmployeeId
+        );
+      } else {
+        rowData.row.selected = "✔️";
+        collectedDataArray.push(collectedDataObject);
+      }
     } else {
-      collectedDataArray.push(collectedDataObject);
+      rows.map((data) => {
+        if (data.employee_id === tickedEmployeeId) {
+          data.selected = "";
+          tickedEmployeeId = "";
+        }
+      });
+      if (collectedDataSingleObject.employee_id === rowData.row.employee_id) {
+        collectedDataSingleObject = {};
+        rowData.row.selected = "";
+        tickedEmployeeId = "";
+      } else {
+        collectedDataSingleObject.name = rowData.row.name;
+        collectedDataSingleObject.email = rowData.row.email;
+        collectedDataSingleObject.employee_id = rowData.row.employee_id;
+        tickedEmployeeId = rowData.row.employee_id;
+        rowData.row.selected = "✔️";
+      }
+      console.log(collectedDataSingleObject);
     }
   };
 
